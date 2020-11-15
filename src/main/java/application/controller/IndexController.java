e9f5b7eb1b5043dde966cf3e8883dac3da33ca38
Jack MacDougall <jackmacdougall@cmail.carleton.ca>
2020-11-15 01:04:04 -0500

    added placeholder home page

---
+++ src/main/java/application/controller/IndexController.java
@@ -0,0 +1,16 @@
+package application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    private IndexController(){}
+
+    @GetMapping(value = "/")
+    public String index(Model model) {
+        return "index";
+    }
+}
